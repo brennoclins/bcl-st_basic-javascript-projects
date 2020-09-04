ecc55e86ff43adff80352cbc9c52f2f5ec8a8c5a
Brenno C. Lins <dev@bcl-st.com.br>
2020-09-04 02:10:15 -0300

    Slider project: Simples tela para passar fotos [ok]

---
+++ 005-slider/scripts.js
@@ -0,0 +1,40 @@
+const prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
+const container = document.querySelector('.images');
+
+let counter = 1;
+
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
+
+function nextSlide() {
+    container.animate(
+        [
+            {opacity: '0.1'},
+            {opacity:'1.0'}
+        ],
+        {duration: 1000, fill: 'forwards'}
+    );
+
+    if(counter === 5){
+        counter = -1;
+    }
+    counter++
+    container.style.backgroundImage = `url(imgs/slider-0${counter}.png)`;
+}
+
+function prevSlide() {
+    container.animate(
+        [
+            {opacity: '0.1'},
+            {opacity:'1.0'}
+        ],
+        {duration: 1000, fill: 'forwards'}
+    );
+
+    if(counter === 0){
+        counter = 4;
+    }
+    counter--
+    container.style.backgroundImage = `url(imgs/slider-0${counter}.png)`;
+}
